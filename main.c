707b51075753f8712ec2965235e9ca5d9f668e51
kassu11 <61124141+kassu11@users.noreply.github.com>
2026-07-28 18:03:16 +0300

    feat: ✨ Add tile textures

--- main.c
+++ main.c
@@ -28,19 +28,50 @@ void init(int bombCount, int gridWidth, int gridHeight, char tiles[], char activ
   UnloadRandomSequence(bombs);
 }
 
-int main(void) {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+void drawTile(Texture texture, int x, int y) {
+  DrawTexturePro(
+    texture, 
+    (Rectangle){.height = texture.width, .width = texture.height, .x = 0, .y = 0},
+    (Rectangle){.height = 48, .width = 48, .x = x, .y = y},
+    (Vector2){.x = 0, .y = 0},
+    0,
+    WHITE
+  );
+}
 
-    int gridWidth = 10, gridHeight = 10, cellSize = 40, gap = 1, bombCount = 10, dead = 0;
+Texture2D loadTexture(const char *fileName) {
+  Image image = LoadImage(fileName);                // Loaded in CPU memory (RAM)
+  Texture2D texture = LoadTextureFromImage(image);  // Image converted to texture, GPU memory (VRAM)
+  UnloadImage(image);                               // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
+
+  return texture;
+}
+
+int main(void) {
+    int gridWidth = 10, gridHeight = 10, cellSize = 48, gap = 1, bombCount = 10, dead = 0;
     char tiles[gridWidth * gridHeight];
     char activity[gridWidth * gridHeight];
+
+    const int screenWidth = (cellSize + gap) * gridWidth - gap;
+    const int screenHeight = (cellSize + gap) * gridHeight - gap;
+
 
     InitWindow(screenWidth, screenHeight, "Mine Sweeper");
 
     SetTargetFPS(60);
 
     init(bombCount, gridWidth, gridHeight, tiles, activity, &dead);
+
+    Texture2D tile0 = loadTexture("textures/tile-0.png");
+    Texture2D tile1 = loadTexture("textures/tile-1.png");
+    Texture2D tile2 = loadTexture("textures/tile-2.png");
+    Texture2D tile3 = loadTexture("textures/tile-3.png");
+    Texture2D tile4 = loadTexture("textures/tile-4.png");
+    Texture2D tile5 = loadTexture("textures/tile-5.png");
+    Texture2D tile6 = loadTexture("textures/tile-6.png");
+    Texture2D tile7 = loadTexture("textures/tile-7.png");
+    Texture2D tile8 = loadTexture("textures/tile-8.png");
+
 
     while (!WindowShouldClose()) {
       if (IsKeyPressed(KEY_R)) {
@@ -120,13 +151,37 @@ int main(void) {
             else {
               DrawRectangle(x * (cellSize + gap), y * (cellSize + gap), cellSize, cellSize, GRAY);
             }
-            if (tiles[y * gridWidth + x] != '0'){
-              DrawText((char []){ tiles[y * gridWidth + x], '\0' }, x * (cellSize + gap), y * (cellSize + gap), cellSize, BLACK);
+            if (tiles[y * gridWidth + x] == '0') {
+              // DrawText((char []){ tiles[y * gridWidth + x], '\0' }, x * (cellSize + gap), y * (cellSize + gap), cellSize, BLACK);
+              drawTile(tile0, x * (cellSize + gap), y * (cellSize + gap));
+            }
+            if (tiles[y * gridWidth + x] == '1') {
+              drawTile(tile1, x * (cellSize + gap), y * (cellSize + gap));
+            }
+            if (tiles[y * gridWidth + x] == '2') {
+              drawTile(tile2, x * (cellSize + gap), y * (cellSize + gap));
+            }
+            if (tiles[y * gridWidth + x] == '3') {
+              drawTile(tile3, x * (cellSize + gap), y * (cellSize + gap));
+            }
+            if (tiles[y * gridWidth + x] == '4') {
+              drawTile(tile4, x * (cellSize + gap), y * (cellSize + gap));
+            }
+            if (tiles[y * gridWidth + x] == '5') {
+              drawTile(tile5, x * (cellSize + gap), y * (cellSize + gap));
+            }
+            if (tiles[y * gridWidth + x] == '6') {
+              drawTile(tile6, x * (cellSize + gap), y * (cellSize + gap));
+            }
+            if (tiles[y * gridWidth + x] == '7') {
+              drawTile(tile7, x * (cellSize + gap), y * (cellSize + gap));
+            }
+            if (tiles[y * gridWidth + x] == '8') {
+              drawTile(tile8, x * (cellSize + gap), y * (cellSize + gap));
             }
           }
         }
       }
-
 
       EndDrawing();
     }
